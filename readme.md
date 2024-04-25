588d1864e2b58fc03c4e16894d5939b107ec392b
Bartosz Osiński <osinskibartosz24@gmail.com>
2024-04-25 20:12:06 +0200

    Readme

---
+++ readme.md
@@ -0,0 +1,17 @@
+# halloween Game
+
+## Ideas
+
+- Wall of garlic falling down
+- Some enemy that is shooting garlic
+- Some kind of puzzles
+- Player can teleport in some limited range (needs mana)
+- Player can somehow hurt enemies
+- Player can find bottles with blood to heal / to be able to teleport
+- Game of light and shadow - moving reflectors, when light get on a vampire he gets hurt
+  - Also, can introduce some kind of mirrors that player can move to reflect light
+- Vampire can possess zombies and control them for some time to solve puzzles
+
+## Assets to use
+
+- https://opengameart.org/content/free-graveyard-platformer-tileset
--- src/mainScene/levels/levels.ts
+++ src/mainScene/levels/levels.ts
@@ -1,20 +1,21 @@
 import { Level } from './Level';
 import Rectangle = Phaser.Geom.Rectangle;
+import Vector2 = Phaser.Math.Vector2;
 
 export const levels: Array<Level> = [
 	{
-		startPosition: new Phaser.Math.Vector2(200, 300),
+		startPosition: new Vector2(200, 300),
 		bounds: new Rectangle(0, -1024/2, 2000, 1024),
 		platforms: [
-			new Phaser.Math.Vector2(0, 0),
-			new Phaser.Math.Vector2(10, -10),
-			new Phaser.Math.Vector2(200, 550-100),
-			new Phaser.Math.Vector2(500, 450-100),
-			new Phaser.Math.Vector2(1000, 500-100),
-			new Phaser.Math.Vector2(1500, 450-100)
+			new Vector2(0, 0),
+			new Vector2(10, -10),
+			new Vector2(200, 550-100),
+			new Vector2(500, 450-100),
+			new Vector2(1000, 500-100),
+			new Vector2(1500, 450-100)
 		],
 		zombies: [
-			new Phaser.Math.Vector2(1000, 376),
+			new Vector2(1000, 376),
 		]
 	}
 ]
