951daff8ebc74e46dc0ee063fb398d58ae497e5f
Bartosz Osiński <osinskibartosz24@gmail.com>
2024-04-29 00:38:36 +0200

    Level 1

--- src/mainScene/levels/levels.ts
+++ src/mainScene/levels/levels.ts
@@ -1,27 +1,6 @@
 import { Level } from './Level';
-import Vector2 = Phaser.Math.Vector2;
+import { level1 } from './level1.ts';
 
 export const levels: Array<Level> = [
-	{
-		startPosition: new Vector2(5, 47),
-		sizeInTiles: {width: 50, height: 50},
-		groundTiles: [
-			{ x: 0, y: 0 },
-			{ x: 1, y: 0 },
-			{ x: 0, y: 49 },
-			{ x: 1, y: 49 },
-			{ x: 2, y: 49 },
-			{ x: 3, y: 49 },
-			{ x: 4, y: 49 },
-			{ x: 5, y: 49 },
-			{ x: 6, y: 49 },
-			{ x: 7, y: 49 },
-			{ x: 8, y: 49 },
-			{ x: 9, y: 49 },
-		],
-		zombies: [
-			new Vector2(9, 48.5),
-		],
-		signs: []
-	}
+	level1
 ]
