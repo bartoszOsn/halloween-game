c99a52deb14bc85d8ab9352c355598123d1aa28a
Bartosz Osiński <osinskibartosz24@gmail.com>
2024-05-02 18:59:46 +0200

    Configure loader baseURL 2

--- src/Bootstrap.ts
+++ src/Bootstrap.ts
@@ -25,7 +25,7 @@ export function bootstrap() {
 		},
 		disableContextMenu: true,
 		loader: {
-			baseURL: import.meta.env.MODE === 'development' ? '' : 'halloween-game/'
+			baseURL: import.meta.env.MODE === 'development' ? '' : 'halloween-game'
 		}
 	});
 
